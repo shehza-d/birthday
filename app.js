b3caff297101cbb466f7e10264f8136b545d181a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-22 15:44:58 +0500

    boiler pale

---
+++ app.js
@@ -0,0 +1,15 @@
+let dateFunction = () => {
+
+	const newDate = new Date()
+	const userDate = document.querySelector('#dateID').value;
+	console.log(userDate)
+	console.log(newDate)
+
+	let age = newDate-userDate;
+	console.log(age)
+	
+
+
+
+
+} 
