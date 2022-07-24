55af4fd7d9bbaeaccf426da63321ddeb8a995b85
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2022-07-24 13:56:05 +0500

    ppush

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ let dateFunction = () => {
 	// let userYear = Number(userDate.slice(0, -6));
 	// console.log(userYear)
 
-	let age2 = today - userDate;
-	// const age = today.getFullYear() - userDate.getFullYear();
+	let age = today - userDate;
+	// const age1 = today.getFullYear() - userDate.getFullYear();
 	//     7                 3 or 10
 	// if (today.getMonth() > userDate.getMonth()) {
 	// 	console.log(`if running`)
@@ -30,15 +30,15 @@ let dateFunction = () => {
 	// 	console.log(`else running`)
 	// 	let ageMonth = userDate.getMonth() - today.getMonth() ;
 	// }
-	console.log(age2)
+	// console.log(age)
 	// console.log(ageMonth)
 
 	// const birthday = `You are ${age} Years, ${ageMonth} and ${ageDay} Old !`;
 
-	// if (age = -1) {
-	// 	alert("error age is greater then current year!");
-	// 	return;
-	// }
+	if (userDate>today) {
+		alert("Age can't be greater then current Date!");
+		return;
+	}
 
 	// console.log(today.getDate())
 
@@ -46,6 +46,24 @@ let dateFunction = () => {
 	// console.log(age)
 
 
+	//converting milisecond into...
+	let second = Math.floor(age / 1000);
+	let minute = Math.floor(second / 60);
+	    second = second % 60;
+	let hour = Math.floor(minute / 60);
+	    minute = minute % 60;
+	let ageDay = Math.floor(hour / 24);
+	    hour = hour % 24;
+	let ageMonth = Math.floor(ageDay / 30);
+	    ageDay = ageDay % 30;
+	let ageYear = Math.floor(ageMonth / 12);
+	    ageMonth = ageMonth % 12;
+//https://gist.github.com/PantheraRed/2e65c48cdfa6fba49473913300cc8b12
 
+	const birthday = `You are ${ageYear} Years, ${ageMonth} Months and ${ageDay} Days Old !`;
+	console.log(birthday)
+	// console.log(year, month, day, hour, minute)
+	
+//calculation days left to next birthday
 
 } 
